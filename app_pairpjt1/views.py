1d63a49324916bd83e7144b678f96191e3c1c4fe
soohofather <enthchal@gmail.com>
2022-09-30 14:32:13 +0900

    2022년 9월 30일 기능 구현 완료

--- app_pairpjt1/views.py
+++ app_pairpjt1/views.py
@@ -23,7 +23,51 @@ def create(request):
 
     title = request.GET.get("title")
     content = request.GET.get("content")
+    created_at = request.GET.get("created_at")
 
     Review.objects.create(title=title, content=content)
+
+    return redirect("pjt1:index")
+
+
+def detail(request, pk):
+
+    content = Review.objects.get(pk=pk)
+
+    context = {
+        "content": content,
+    }
+
+    return render(request, "app_pairpjt1/detail.html", context)
+
+
+def delete(request, pk):
+
+    delete = Review.objects.get(pk=pk)
+
+    delete.delete()
+
+    return redirect("pjt1:index")
+
+
+def edit(request, pk):
+
+    edit_content = Review.objects.get(pk=pk)
+
+    context = {
+        "edit_content": edit_content,
+    }
+
+    return render(request, "app_pairpjt1/edit.html", context)
+
+
+def update(request, pk):
+
+    old_content = Review.objects.get(pk=pk)
+
+    old_content.title = request.GET.get("title")
+    old_content.content = request.GET.get("content")
+
+    old_content.save()
 
     return redirect("pjt1:index")
